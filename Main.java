f8c5fdbbd7ec7ff9ecf350011cc33f1943c762fb
Evgeniy Parfenyuk <info.jacktiger@gmail.com>
2022-05-11 12:55:31 +0300

    again change file main

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main{
     public static void main(String... args){
         System.out.println("Hello world!!!!");
         System.out.println("Privet, kak dela?");
+        int[] s = new int[10];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = i;
+            System.out.println(i);
+        }
     }
 }
